d5eadf71eb9427f5fb5136686f4a1c10f5bc8e59
call-me-matt <matthias@butler>
2020-07-30 22:43:02 +0200

    send reminder to all whitelisted chats
    Signed-off-by: call-me-matt <matthias@butler>

--- index.js
+++ index.js
@@ -6,44 +6,50 @@
 
 const functionSendImageReminder = (interface) => {
     
-    var config;
     const REMINDER_WAIT_DAYS = 14;
+    const REMINDER_MESSAGE = "hey hey, please feed me with new photos 🤖";
+
+    var config;
     var telegram = null;
     var friendlyReminder = 0;
 
-	interface.registerListener('teleFrame-ready', teleFrameObjects => {
-	  telegram = teleFrameObjects.bot.telegram;
-	  config = teleFrameObjects.config;
-	  resetTimer();
-	});
-
-  /**
-   * function that resets the reminder timer
-   */
-  const resetTimer = () => {
-    // remove running reminder
-    clearTimeout(friendlyReminder);
-    // trigger new reminder
-    const remindTime = REMINDER_WAIT_DAYS * 1000 * 60 * 60 * 24;
-    friendlyReminder = setTimeout(() => {
-        interface.logger.info("sending out a friendly reminder");
-        telegram.sendMessage(config.whitelistChats[0], "hey hey, please feed me with new photos 🤖");
-        // keep on reminding
-        resetTimer();
-    }, remindTime);
-  };
-
-  /**
-   * triger a timer reset on new picture
-   */
-  interface.registerListener(['newImage'],
-    () => {
+    interface.registerListener('teleFrame-ready', teleFrameObjects => {
+        telegram = teleFrameObjects.bot.telegram;
+        config = teleFrameObjects.config;
         resetTimer();
     });
 
+    /**
+     * function that resets the reminder timer
+     */
+    const resetTimer = () => {
+        // remove running reminder
+        clearTimeout(friendlyReminder);
+        // trigger new reminder
+        const remindTime = REMINDER_WAIT_DAYS * 1000 * 60 * 60 * 24;
+        friendlyReminder = setTimeout(() => {
+            interface.logger.info("sending out a friendly reminder");
+            for (i in config.whitelistChats) {
+                telegram.sendMessage(config.whitelistChats[i], REMINDER_MESSAGE)
+                .catch(error => {
+                      interface.logger.error(error.message);
+                  });
+            }
+            // keep on reminding
+            resetTimer();
+        }, remindTime);
+    };
+
+    /**
+     * triger a timer reset on new picture
+     */
+    interface.registerListener(['newImage'],
+        () => {
+            resetTimer();
+        });
 };
 
 /*************** DO NOT EDIT THE LINE BELOW ***************/
 if (typeof module !== "undefined") {
-  module.exports = functionSendImageReminder;
+    module.exports = functionSendImageReminder;
 }
